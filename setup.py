e0e7ff84ba6af298f3edbffb72923a785feb0697
Jarod Luebbert <jarodluebbert@gmail.com>
2011-12-12 15:18:56 -0500

    Bump version.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Github',
-    version='0.1.2',
+    version='0.1.3',
     url='http://github.com/jarodl/flask-github',
     license='MIT',
     author='Jarod Luebbert',
